35be34a913a9dce6ca638e8c5c7771d8858a50b4
gladysndich <gladys@DESKTOP-TMOKO1H.localdomain>
2022-07-18 17:57:17 +0300

    Done

---
+++ Introduction-to-React-Quiz/README.md
@@ -0,0 +1,28 @@
+# Introduction to React Quiz
+
+## Author :
+Lorenah Mbogo
+
+# Getting Started with the Quiz
+
+Fork [this repository](https://github.com/Skylar-Lorena/Introduction-to-React-Quiz.git)
+
+On your terminal git clone https://github.com/Skylar-Lorena/Introduction-to-React-Quiz.git
+
+cd `react-week1-quiz`
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### Follow instructions in source.md to complete the Quiz`
+
+### Commit and Push Changes
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
---
+++ Introduction-to-React-Quiz/src/App.js
@@ -0,0 +1,85 @@
+import React from "react";
+import './App.css';
+
+
+/**
+  Challenge: Display all users to the browser
+**/
+
+const users = [
+  { name: "John Doe", id: 1 },
+  { name: "Jane Doe", id: 2 },
+  { name: "Billy Doe", id: 3 }
+];
+
+// comment this out after completion and uncomment code below it to proceed
+function Child() {
+  return <div>This is children content</div>;
+}
+/**
+  Challenge: Uncomment this code to complete quiz
+
+function Child() {
+  return (
+    <>
+      <div>Child</div>
+      <button>Change Parent Value</button>
+    </>
+  );
+}
+
+function Parent() {
+  const [value, setValue] = React.useState(
+    "I need to be updated from my child"
+  );
+
+  return (
+    <>
+      <h3>Update Parent State Challenge (Using Callback)</h3>
+      <div className="wrapper">
+        <div>Parent</div>
+        <div className="box-wrapper">{value}</div>
+      </div>
+
+      <div className="wrapper">
+        <Child />
+      </div>
+    </>
+  );
+}
+Uncomment this to tackle quiz
+*/
+
+// Comment out after completion
+function Parent() {
+  return (
+    <div>
+      <h3>Parent Component</h3>
+    </div>
+  );
+}
+// Comment above code after completion
+
+function App() {
+  const [hide,setHide] = React.useState(true);
+  const hideContent=()=>{
+    setHide(!hide);
+  }
+  return (
+    
+    <>
+    
+    <img src="https://c.tenor.com/QahtlpcnGOMAAAAC/dance-funny-dance.gif" className="App-logo" alt="logo" />
+      <p>JSX is Cool!</p>
+      <h3>User names</h3>
+      <ul className="user">{users.map((item)=><li key={item.id}>{item.name}</li>)}</ul>
+      
+      <button onClick={hideContent}>Hide Element Below</button>
+
+      <div>Toggle Challenge</div>
+      {hide?<Parent><Child /></Parent>:null}
+    </>
+  );
+}
+
+export default App;
